0647bf2c825fc069750b93aa7a71bcce00f0b7d5
JayantSravan <jayant.sravan@gmail.com>
2021-10-19 22:39:28 -0400

    Added route OTP sectino

--- application.py
+++ application.py
@@ -27,7 +27,22 @@ with st.spinner('Bringing you awesome...'):
 
 # introduction page
 def introduction(otp_dataset, stops_dataset):
-    st.title('Introduction')
+    st.title('Pittsburgh Port Authority On-time Performance (OTP)')
+    
+    st.header('Introduction')
+    "A city's public transportation system is a testament to how well the city is planned. Pittsburgh is famous for its efficient, well-connected, and punctual public transportation system. As an intrigued visitor to this city, I wanted to understand (and also let the users visualize) using data how well this claim holds."
+
+    "The goal of this project is to give the user an understanding of the distribution of On-time Performance of Pittsburgh Port Authority transit vehicles over time and locations in the city. A transit vehicle is said to be on time if it reaches a given destination within one minute of its scheduled time."
+
+    "To my surprise, data suggested that Pittsburgh public transit has remarkable punctuality (atleast towards the center of the city). And they have been very consistent about this throughout the past 3 years and across the garages."
+    
+    st.header('Will my transit be on time?')
+    "I have baked in 'Will my transit be on time?' functionality in the application (look in navigation). Hope this helps some users be informed about any possible delays."
+    
+    st.header('Data used')
+    "All the data used is obtained from the Western Pennsylvania Regional Data Center (WPRDC). Specifically, I used the 'Monthly OTP by Route' and 'Monthly_Updating_Bus_Stop_Usage' datasets."
+    st.markdown('Monthly OTP by Route - https://data.wprdc.org/dataset/port-authority-monthly-average-on-time-performance-by-route/resource/00eb9600-69b5-4f11-b20a-8c8ddd8cfe7a', unsafe_allow_html=True)
+    st.markdown('Monthly_Updating_Bus_Stop_Usage - https://data.wprdc.org/dataset/port-authority-transit-stop-usage/resource/3f40b94b-4ac4-48f1-8c61-8439d2d2f420', unsafe_allow_html=True)
 
 # geographic distribution page
 def geographic_distribution(otp_dataset, stops_dataset):
@@ -36,25 +51,25 @@ def geographic_distribution(otp_dataset, stops_dataset):
     st.header('Filter the data')
 
     # Filter the otp data
-    otp_dataset = otp_dataset[otp_dataset['on_time_percent']>0]
-
-    dayFilter = st.multiselect('1. Day of Week',['WEEKDAY', 'SAT.', 'SUN.'])
-    if not dayFilter:
-        dayFilter = ['WEEKDAY', 'SAT.', 'SUN.']
-    otp_dataset_filtered = filterDatasets(otp_dataset, 'day_type', dayFilter)
-    otp_dataset_filtered['day_weight'] = otp_dataset_filtered.apply(lambda row: 5 if row['day_type'] == 'WEEKDAY' else 1, axis = 1)
-    otp_dataset_filtered['otp(weighted)'] = otp_dataset_filtered['day_weight'] * otp_dataset_filtered['on_time_percent']
+    otp_dataset_filtered = otp_dataset[otp_dataset['on_time_percent']>0]
 
     garages = ['Ross', 'Collier', 'East Liberty', 'East Liberty/West Mifflin']
-    garageFilter = st.multiselect('2. Garage', garages)
+    garageFilter = st.multiselect('1. Garage', garages)
     if not garageFilter:
         garageFilter = garages
     otp_dataset_filtered = filterDatasets(otp_dataset_filtered, 'current_garage', garageFilter)
+
+    dayFilter = st.multiselect('2. Day of Week',['WEEKDAY', 'SAT.', 'SUN.'])
+    if not dayFilter:
+        dayFilter = ['WEEKDAY', 'SAT.', 'SUN.']
+    otp_dataset_filtered = filterDatasets(otp_dataset_filtered, 'day_type', dayFilter)
+    otp_dataset_filtered['day_weight'] = otp_dataset_filtered.apply(lambda row: 5 if row['day_type'] == 'WEEKDAY' else 1, axis = 1)
+    otp_dataset_filtered['otp(weighted)'] = otp_dataset_filtered['day_weight'] * otp_dataset_filtered['on_time_percent']   
     
-    timeDuration = st.selectbox('3. How recent data do you want to consider?', ('All time', 'Last 9 months', 'Last 12 months', 'Last 18 months'))
-    if timeDuration != 'All time':
-        months = int(timeDuration.split(' ')[1])
-        otp_dataset_filtered = filterDatasets(otp_dataset_filtered, 'month_start', datetime.now() - relativedelta(months=months))
+    # timeDuration = st.selectbox('3. How recent data do you want to consider?', ('All time', 'Last 9 months', 'Last 12 months', 'Last 18 months'))
+    # if timeDuration != 'All time':
+    #     months = int(timeDuration.split(' ')[1])
+    #     otp_dataset_filtered = filterDatasets(otp_dataset_filtered, 'month_start', datetime.now() - relativedelta(months=months))
 
     # filter the stops data
     stops = stops_dataset.drop_duplicates('stop_name')[['stop_name', 'latitude', 'longitude', 'routes_ser']]
@@ -96,6 +111,7 @@ def geographic_distribution(otp_dataset, stops_dataset):
                     get_weight = "on_time_percent",
                     cell_size = 400,
                     elevation_scale = 10,
+                    radius = 200,
                     opacity = 0.7
                 ),
                 pdk.Layer(
@@ -112,7 +128,7 @@ def geographic_distribution(otp_dataset, stops_dataset):
         )
     )
     f"**Fig 1:** *Geographic Distribution of OTP in Pittsburgh city during {', '.join(dayFilter)} \
-        for {', '.join(garages)} garages (resresented as black bars) in {timeDuration.lower()}.*"
+        for {', '.join(garages)} garages (resresented as black bars).*"
 
     showDistributionData = st.checkbox("Show Data")
     if showDistributionData:
@@ -169,9 +185,31 @@ def time_distribution(otp_dataset, stops_dataset):
         column_values = otp_dataset_filtered['current_garage'].unique()
         create_and_render_chart(otp_dataset_filtered, 'current_garage', column_values)
 
+# bus on time
+def bus_on_time(otp_dataset, stops_dataset):
+    st.title('Will my transit be on time?')
+    otp_dataset = otp_dataset[otp_dataset['on_time_percent']>0]
+    otp_dataset_filtered = otp_dataset.copy()
+    otp_dataset_filtered['month_start'] = pd.to_datetime(otp_dataset_filtered['month_start'], infer_datetime_format=True)
+    globalOtpMean = otp_dataset_filtered['on_time_percent'].mean()
+    column_values = list(otp_dataset_filtered['route'].unique())
+    if column_values.count(np.nan):
+        column_values.remove(np.nan)
+    route = st.selectbox('Select your route', column_values)
+    otp_by_route = otp_dataset_filtered[otp_dataset_filtered['route'] == route]
+    meanOtp = otp_by_route['on_time_percent'].mean()
+    betterBy = (meanOtp - globalOtpMean)/globalOtpMean * 100
+    st.metric(label=f"Average OTP of route {route}", value=f"{int(meanOtp * 100)}%", delta=f"{int(betterBy)}%")
+    "* the delta shown above is in comparison to the average OTP of all transits"
+
+def source():
+    
+    st.write("This application was built as a part of my assignment for Interactive Data Science (05-839) course at Carnegie Mellon University.")
+    "For any queries or suggestions, email me at jtamarap@cs.cmu.edu"
+    st.markdown("Link to the github repository for this project - https://github.com/JayantSravan/assignment-2-JayantSravan", unsafe_allow_html=True)
 # Sidebar and navigation
 st.sidebar.title('Navigation')
-pages = ['Introduction', 'Geographic distribution of OTP', 'Time distribution of OTP']
+pages = ['Introduction', 'Geographic distribution of OTP', 'Time distribution of OTP', 'Will my transit be on time?', 'Source']
 page = st.sidebar.radio('Go to:', pages)
 if page == pages[0]:
     introduction(otp_dataset, stops_dataset)
@@ -179,3 +217,7 @@ elif page == pages[1]:
     geographic_distribution(otp_dataset, stops_dataset)
 elif page == pages[2]:
     time_distribution(otp_dataset, stops_dataset)
+elif page == pages[3]:
+    bus_on_time(otp_dataset, stops_dataset)
+elif page == pages[4]:
+    source()
